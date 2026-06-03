23e3fade02630b1c01957c0a9b2101db8a0ea8f6
AmateurChess <yannik@hess-bc.de>
2026-06-03 17:12:23 +0000

    Fix: Add mising SessionLocal definition in models.py

--- app/models.py
+++ app/models.py
@@ -11,3 +11,11 @@ class Prediction(Base):
     model_version = Column(String, nullable=False)
     created_at = Column(DateTime,
     default=datetime.utcnow)
+
+
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
